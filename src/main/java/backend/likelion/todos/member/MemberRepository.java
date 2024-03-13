4a0a83e719cf39962f8eef001a60af82dce30d8a
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-03-13 16:49:03 +0900

    feat: Member TODO 구현

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -14,14 +14,18 @@ public class MemberRepository {
     // 멤버를 저장소에 저장하고 저장된 멤버를 반환합니다.
     public Member save(Member member) {
         // TODO [1단계] member의 id를 설정하세요.
+        member.setId(id);
+        id++
         // TODO [1단계] members 맵에 member를 추가하세요.
+        members.put(member.getId(), member);
         // TODO [1단계] member를 반환하세요.
-        return null;
+        return member;
     }
 
     // 주어진 id에 해당하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findById(Long id) {
         // TODO [1단계] id를 이용하여 members 맵에서 멤버를 찾으세요.
+        
         // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
         return null;
     }
