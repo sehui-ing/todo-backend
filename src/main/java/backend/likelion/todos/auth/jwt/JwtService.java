ab2953a6800d62f082a2604b2dc3ffdcc1112d9f
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-03-19 22:34:24 +0900

    "feat: createToken 및 extractMemberId 매우 일부 구현"

--- src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
+++ src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
@@ -25,14 +25,30 @@ public class JwtService {
     // 회원 ID를 기반으로 JWT 토큰을 생성합니다.
     public String createToken(Long memberId) {
         // TODO [5단계] 현재 시간과 설정된 만료 시간을 사용하여 만료 날짜를 설정하세요.
+        Date currentDate = new Date();
+        Date endDate = new Date(currentDate.getTime() + accessTokenExpirationDayToMills);
+
         // TODO [5단계] memberId를 클레임으로 추가하세요.
+        String token = ??
+
         // TODO [5단계] 설정된 알고리즘으로 토큰을 서명하고 반환하세요.
-        return null;
+        return token;
     }
 
     // 토큰에서 회원 ID를 추출합니다.
     public Long extractMemberId(String token) {
         // TODO [5단계] 알고리즘을 사용해 토큰의 유효성을 검증하고, "memberId" 클레임에서 회원 ID를 추출하세요. 유효하지 않은 경우 "유효하지 않은 토큰입니다." 메시지와 함께 UnAuthorizedException을 발생시키세요.
+        if (??) {
+            ??
+        } else {
+            throw new UnAuthorizedException("유효하지 않은 토큰입니다.");
+        }
+
+        try {
+            ??
+        } catch (??) {
+            throw new UnAuthorizedException("유효하지 않은 토큰입니다.");
+        }
         return null;
     }
 }
