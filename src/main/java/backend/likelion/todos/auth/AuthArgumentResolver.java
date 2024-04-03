445d7f8e00e1b116202908e669db0a910670cd75
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-04-03 17:13:33 +0900

    "feat: resolveArgument 메서드 구현"

--- src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
+++ src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
@@ -21,9 +21,7 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     public boolean supportsParameter(MethodParameter parameter) {
         // TODO [6단계] parameter가 @Auth 어노테이션을 갖고 있고, 파라미터 타입이 Long.class인 경우 true를 반환하는 조건을 구현하세요.
         if (parameter.hasParameterAnnotation(Auth.class)) {
-            if (parameter.getParameterType().equals(Long.class)) {
-                return true;
-            }
+            return parameter.getParameterType().equals(Long.class);
         }
         return false;
     }
@@ -36,11 +34,13 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
             WebDataBinderFactory binderFactory
     ) {
         // TODO [6단계] webRequest로부터 accessToken을 추출하고, jwtService를 사용하여 memberId를 추출하여 반환하는 로직을 구현하세요.
-        return null;
+        String accessToken = extractAccessToken(webRequest);
+        return jwtService.extractMemberId(accessToken);
     }
 
     private static String extractAccessToken(NativeWebRequest request) {
         // TODO [6단계] request 헤더에서 "Authorization" 헤더 값을 추출하여 "Bearer "로 시작하는 accessToken을 반환하세요. 유효하지 않을 경우 "로그인 후 접근할 수 있습니다." 메시지와 함께 UnAuthorizedException을 발생시키는 로직을 구현하세요.
+
         return null;
     }
 
