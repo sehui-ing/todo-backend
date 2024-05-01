d045787c8844c67b886e9cbabdcca95043630547
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-05-01 23:04:43 +0900

    "feat: GoalService의 save 메서드 구현"

--- src/main/java/backend/likelion/todos/goal/GoalService.java
+++ src/main/java/backend/likelion/todos/goal/GoalService.java
@@ -4,6 +4,8 @@ import backend.likelion.todos.common.NotFoundException;
 import backend.likelion.todos.member.Member;
 import backend.likelion.todos.member.MemberRepository;
 import java.util.List;
+
+import backend.likelion.todos.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +18,15 @@ public class GoalService {
 
     public Long save(String name, String color, Long memberId) {
         // TODO [8단계] memberId로 회원을 조회하고, 조회에 실패하면 "회원 정보가 없습니다." 예외를 발생시키세요.
+        if (memberRepository.findById(memberId).isEmpty()) {
+            throw new NotFoundException("회원 정보가 없습니다.");
+        }
+
         // TODO [8단계] 조회된 회원 정보를 사용하여 새 Goal 객체를 생성하세요.
+        Goal goal = new Goal(name, color, memberRepository.findById(memberId).get());
+
         // TODO [8단계] 생성된 Goal 객체를 goalRepository에 저장하고, 저장된 Goal의 ID를 반환하세요.
-        return null;
+        return goalRepository.save(goal).getId();
     }
 
     public void update(Long goalId, String name, String color, Long memberId) {
