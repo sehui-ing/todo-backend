31158572379616f7ab24f282c5a23a72de21c098
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-05-01 23:25:48 +0900

    "feat: GoalService의 delete 메서드 구현"

--- src/main/java/backend/likelion/todos/goal/GoalService.java
+++ src/main/java/backend/likelion/todos/goal/GoalService.java
@@ -45,9 +45,16 @@ public class GoalService {
 
     public void delete(Long goalId, Long memberId) {
         // TODO [8단계] memberId로 회원을 조회하고, 조회에 실패하면 "회원 정보가 없습니다." 예외를 발생시키세요.
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException("회원 정보가 없습니다."));
+
         // TODO [8단계] goalId로 목표(Goal)를 조회하고, 조회에 실패하면 "목표 정보가 없습니다." 예외를 발생시키세요.
+        Goal goal = goalRepository.findById(goalId).orElseThrow(() -> new NotFoundException("목표 정보가 없습니다."));
+
         // TODO [8단계] 조회된 Goal의 회원 정보가 입력된 memberId와 일치하는지 검증하세요.
+        goal.validateMember(member);
+
         // TODO [8단계] 검증이 완료되면 Goal을 goalRepository에서 삭제하세요.
+        goalRepository.delete(goal);
     }
 
     public List<GoalResponse> findAllByMemberId(Long memberId) {
